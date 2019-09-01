11a5419e3530a5b3887133b9c55a2797f58ca2be
karthi031000 <48919893+karthi031000@users.noreply.github.com>
2019-09-01 11:02:20 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,8 +1,14 @@
 n = int(input())
-ch = map(int,input().split())
-lst = ch.split()
-ln = 2
-for i in range(0,len(lst),ln):
-               yield[i:i + ln]
-  
-  
+lst = list(map(int,input().split()))
+lst1 = []
+for i in range(0,n,2):
+    temp = lst[i]
+    lst[i] = lst[i+1]
+    lst[i+1] = temp
+    a = lst[i]
+    b = lst[i+1]
+    lst1.extend([a,b])
+    if(i % 2 == 0 and n % 2 != 0):
+        lst1.append(lst[-1])
+        break
+print(lst1)
