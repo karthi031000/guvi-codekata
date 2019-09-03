c298e65c8d6a183de9ca85ce3f8d6877198d1f3b
karthi031000 <48919893+karthi031000@users.noreply.github.com>
2019-09-03 11:22:11 +0530

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,9 +1,7 @@
 n = int(input())
-#if(n == 1):
-# print("no")
 for i in range(2,n):
-  if(n % i != 0):
-    print("no")
-  else:
+  if(i % 2 == 0):
     print("yes")
     break
+else:
+  print("no")
