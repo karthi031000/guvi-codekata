6e7cf3457e524178d9ae098be32367836099a6e1
karthi031000 <48919893+karthi031000@users.noreply.github.com>
2019-09-08 14:02:26 +0530

    Update smallest_largest.py

--- smallest_largest.py
+++ smallest_largest.py
@@ -1,8 +1,7 @@
 N = int(input())
 lst = list(map(int,input().split()))
-a = min(lst)
-b = max(lst)
-pos1 = lst.index(a) + 1
-pos2 = lst.index(b) + 1
+mini = min(lst)
+maxi = max(lst)
+pos1 = index(mini) + 1
+pos2 = index(maxi) + 1
 print(pos1,pos2)
-
